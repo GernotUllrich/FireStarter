009410781742b07e5be766b4996800c760fe6fce
Gernot Ullrich <gernot.ullrich@gmx.de>
2015-12-03 20:03:53 +0100

    FireStarter with UlangoTV UserAppStore

--- FireStarterProject/FireStarter/src/main/java/de/belu/firestarter/gui/UpdaterAppsAdapter.java
+++ FireStarterProject/FireStarter/src/main/java/de/belu/firestarter/gui/UpdaterAppsAdapter.java
@@ -2,6 +2,7 @@ package de.belu.firestarter.gui;
 
 import android.app.Activity;
 import android.content.Context;
+import android.util.JsonReader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,42 +10,89 @@ import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import de.belu.firestarter.R;
 import de.belu.firestarter.tools.FireStarterUpdater;
 import de.belu.firestarter.tools.KodiUpdater;
 import de.belu.firestarter.tools.SettingsProvider;
+import de.belu.firestarter.tools.UlangoTVUpdater;
 import de.belu.firestarter.tools.Updater;
 
 /**
  * Adapter that lists all installed apps
  */
-public class UpdaterAppsAdapter extends BaseAdapter
-{
+public class UpdaterAppsAdapter extends BaseAdapter {
     Activity mActivity;
     private List<Updater> mUpdaterList;
+    String androidPackagesUrl = "https://ulango.tv/android.json";
+    private List<String> ulangoPackages = null;
+    UlangoUpdaterRunnable updateRunnable = null;
+    class UlangoUpdaterRunnable implements Runnable {
+
+        private boolean listsDone = false;
+
+        Thread t;
+
+        public boolean getListsDone() {
+            return listsDone;
+        }
+
+        UlangoUpdaterRunnable() {
+            t = new Thread(this);
+            t.start();
+        }
+
+        @Override
+        public void run() {
+            try {
+                ulangoPackages = getPackagesFromUlangoAndroidUserAppStore();
+                Iterator<String> iterator = ulangoPackages.iterator();
+                while (iterator.hasNext()) {
+                    String names = iterator.next();
+                    String[] separated = names.split("\\|");
+                    mUpdaterList.add(new UlangoTVUpdater(separated[0], separated[1]));
+                }
+                synchronized (this) {
+                    listsDone = true;
+                    notifyAll();
+                }
+            } catch (Exception e) {
+            }
+        }
+    }
 
     /**
      * Create new UpdaterAppsadapter
      */
-    public UpdaterAppsAdapter(Activity activity)
-    {
+    public UpdaterAppsAdapter(Activity activity) {
         // Set context
         mActivity = activity;
 
         // Set list of updaters
         mUpdaterList = new ArrayList<>();
-        mUpdaterList.add(new FireStarterUpdater());
+        //mUpdaterList.add(new FireStarterUpdater());
         mUpdaterList.add(new KodiUpdater(activity));
+        updateRunnable = new UlangoUpdaterRunnable();
     }
 
     /**
      * @return Count of installed apps
      */
-    public int getCount()
-    {
+
+    public int getCount() {
+        try {
+            updateRunnable.t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return mUpdaterList.size();
     }
 
@@ -52,39 +100,44 @@ public class UpdaterAppsAdapter extends BaseAdapter
      * @param position Position of item to be returned
      * @return Item on position
      */
-    public Object getItem(int position)
-    {
+
+    public Object getItem(int position) {
+        try {
+            updateRunnable.t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return mUpdaterList.get(position);
     }
 
     /**
      * Currently not used..
      */
-    public long getItemId(int position)
-    {
+    public long getItemId(int position) {
         return position;
     }
 
     /**
      * @return View of the given position
      */
-    public View getView(int position, View convertView, ViewGroup parent)
-    {
+    public View getView(int position, View convertView, ViewGroup parent) {
         // Get act updater
+        try {
+            updateRunnable.t.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         final Updater actUpdater = mUpdaterList.get(position);
 
         // Inflate layout
         View rootView;
 
-        if (convertView == null)
-        {
+        if (convertView == null) {
             LayoutInflater inflater = (LayoutInflater) mActivity.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             rootView = new View(mActivity);
             rootView = inflater.inflate(R.layout.appupdateritemlayout, parent, false);
 
-        }
-        else
-        {
+        } else {
             rootView = (View) convertView;
         }
 
@@ -99,8 +152,7 @@ public class UpdaterAppsAdapter extends BaseAdapter
         // Set latest version
         final TextView textViewLatestVersion = (TextView) rootView.findViewById(R.id.latestVersion);
         String latestVersion = actUpdater.getLatestVersion();
-        if(latestVersion == null)
-        {
+        if (latestVersion == null) {
             latestVersion = mActivity.getResources().getString(R.string.update_hitcheckfor);
         }
         textViewLatestVersion.setText(latestVersion);
@@ -108,29 +160,20 @@ public class UpdaterAppsAdapter extends BaseAdapter
         // Create an UpdaterDialogHandler
         final UpdaterDialogHandler updaterDialogHandler = new UpdaterDialogHandler(mActivity, actUpdater);
         actUpdater.DialogHandler = updaterDialogHandler;
-        updaterDialogHandler.setCheckForUpdateFinishedListener(new Updater.OnCheckForUpdateFinishedListener()
-        {
+        updaterDialogHandler.setCheckForUpdateFinishedListener(new Updater.OnCheckForUpdateFinishedListener() {
             @Override
-            public void onCheckForUpdateFinished(String message)
-            {
-                if (actUpdater.getLatestVersion() != null)
-                {
-                    if (actUpdater.isVersionNewer(actUpdater.getCurrentVersion(mActivity), actUpdater.getLatestVersion()))
-                    {
+            public void onCheckForUpdateFinished(String message) {
+                if (actUpdater.getLatestVersion() != null) {
+                    if (actUpdater.isVersionNewer(actUpdater.getCurrentVersion(mActivity), actUpdater.getLatestVersion())) {
                         textViewLatestVersion.setText(actUpdater.getLatestVersion() + " - " + mActivity.getResources().getString(R.string.update_foundnew));
 
-                        if(actUpdater instanceof FireStarterUpdater)
-                        {
+                        if (actUpdater instanceof FireStarterUpdater) {
                             AppActivity.LATEST_APP_VERSION = actUpdater.getLatestVersion();
                         }
-                    }
-                    else
-                    {
+                    } else {
                         textViewLatestVersion.setText(actUpdater.getLatestVersion() + " - " + mActivity.getResources().getString(R.string.update_foundnotnew));
                     }
-                }
-                else
-                {
+                } else {
                     textViewLatestVersion.setText(message);
                 }
             }
@@ -138,25 +181,20 @@ public class UpdaterAppsAdapter extends BaseAdapter
 
         // Set the button onclicks
         Button checkUpdateButton = (Button) rootView.findViewById(R.id.buttonCheckForUpdate);
-        checkUpdateButton.setOnClickListener(new View.OnClickListener()
-        {
+        checkUpdateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 updaterDialogHandler.checkForUpdate();
             }
         });
 
         Button updateButton = (Button) rootView.findViewById(R.id.buttonUpdate);
-        updateButton.setOnClickListener(new View.OnClickListener()
-        {
+        updateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v)
-            {
+            public void onClick(View v) {
                 updaterDialogHandler.performUpdate();
 
-                if(actUpdater instanceof FireStarterUpdater)
-                {
+                if (actUpdater instanceof FireStarterUpdater) {
                     SettingsProvider settings = SettingsProvider.getInstance(mActivity);
                     settings.setHaveUpdateSeen(false);
                 }
@@ -165,4 +203,54 @@ public class UpdaterAppsAdapter extends BaseAdapter
 
         return rootView;
     }
+
+    private List<String> getPackagesFromUlangoAndroidUserAppStore() {
+        ArrayList<String> result = new ArrayList<String>();
+        InputStream is = null;
+        try {
+            URL url = new URL(androidPackagesUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            int response = conn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                is = conn.getInputStream();
+                JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                reader.beginArray();
+                while (reader.hasNext()) {
+                    String package_name = null;
+                    String app_name = null;
+                    reader.beginObject();
+                    while (reader.hasNext()) {
+                        String name = reader.nextName();
+                        if (name.equals("app_name")) {
+                            app_name = reader.nextString();
+                        } else if (name.equals("package_name")) {
+                            package_name = reader.nextString();
+                        } else {
+                            reader.skipValue();
+                        }
+                    }
+                    if (package_name != null) {
+                        result.add(app_name + "|" + package_name);
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (is != null) {
+                try {
+                    is.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return result;
+    }
 }
+
---
+++ FireStarterProject/FireStarter/src/main/java/de/belu/firestarter/tools/UlangoTVUpdater.java
@@ -0,0 +1,89 @@
+package de.belu.firestarter.tools;
+
+import android.content.Context;
+import android.util.JsonReader;
+
+import org.json.JSONException;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Update class for FireStarter
+ */
+public class UlangoTVUpdater extends Updater {
+    /**
+     * Update URL where updated versions are found
+     */
+    private String mUpdateUrl;
+    private String mPackageName;
+    private String mAppName;
+
+    @Override
+    public String getAppName() {
+        return "UlangoTV";
+    }
+    /** Constructor to get Context */
+    public UlangoTVUpdater(String app_name, String package_name)
+    {
+        mPackageName = package_name;
+        mAppName = app_name;
+        mUpdateUrl = "https://ulango.tv/android/" + package_name + "/releases.json";
+    }
+
+    @Override
+    public String getPackageName(Context context) {
+        return mPackageName;
+    }
+
+    @Override
+    public Boolean isVersionNewer(String oldVersion, String newVersion) {
+        // Use standard check
+        return isVersionNewerStandardCheck(oldVersion, newVersion);
+    }
+
+    @Override
+    protected void updateLatestVersionAndApkDownloadUrl() throws Exception {
+        InputStream is = null;
+
+        try {
+            URL url = new URL(mUpdateUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+            conn.connect();
+            int response = conn.getResponseCode();
+            if (response == HttpURLConnection.HTTP_OK) {
+                is = conn.getInputStream();
+                JsonReader reader = new JsonReader(new InputStreamReader(is, "UTF-8"));
+                reader.beginArray();
+                while (reader.hasNext()) {
+                    reader.beginObject();
+                    while (reader.hasNext()) {
+                        String name = reader.nextName();
+                        if (name.equals("version")) {
+                            mLatestVersion = "v" + reader.nextString();
+                        } else if (name.equals("url")) {
+                            mApkDownloadUrl = reader.nextString();
+                            break;
+                        } else {
+                            reader.skipValue();
+                        }
+                    }
+                }
+            }
+        } finally {
+            if (is != null) {
+                is.close();
+            }
+        }
+
+        if (mApkDownloadUrl == null) {
+            throw new JSONException("No .apk download URL found");
+        }
+    }
+}
